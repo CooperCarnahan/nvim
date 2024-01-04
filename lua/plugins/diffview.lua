7042399610cc644a0de79a2d5f0c056998e19aae
Carnahan <cooper.carnahan@hp.com>
2024-01-04 14:36:59 -0600

    feat(diffview): add diffview plugin and keymaps

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gdb", "<cmd>DiffviewFileHistory<cr>", desc = "Open Git Branch Diff History" },
+    { "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "Open Git History for Current File" },
+  },
+  config = function() end,
+}
