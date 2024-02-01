981b16dda9eda08581bc1c23a7276f995b208fe8
Carnahan <cooper.carnahan@hp.com>
2024-02-01 11:01:28 -0600

    feat(telescope): add zoxide extension for switching dirs quickly

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "jvgrootveld/telescope-zoxide",
+  },
   keys = {
     { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>st", "<cmd>Telescope tags<cr>", desc = "Find tags" },
+    { "<leader>sz", "<cmd> Telescope zoxide<cr>", desc = "Telescope Zoxide" },
   },
   opts = {
     pickers = {
       tags = {
         only_sort_tags = true,
       },
+    },
+    extensions = {
+      zoxide = {},
     },
   },
 }
