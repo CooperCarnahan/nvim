23525b708803444fc3439e71a723021972e8be29
Carnahan <cooper.carnahan@hp.com>
2024-02-15 09:32:50 -0600

    fix: ts-node-action keymap conflict with window nav

--- lua/plugins/ts-node-action.lua
+++ lua/plugins/ts-node-action.lua
@@ -2,7 +2,7 @@ return {
   "ckolkey/ts-node-action",
   dependencies = { "nvim-treesitter" },
   opts = {},
-  keys = { { "<c-k>", "<cmd>NodeAction<cr>", desc = "Trigger Node Action" } },
+  keys = { { "gk", "<cmd>NodeAction<cr>", desc = "Trigger Node Action" } },
   config = function()
     require("ts-node-action").setup()
   end,
