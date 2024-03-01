e79a0f0edef6be2f6522493ea4d0240c0cf9ebea
Carnahan <cooper.carnahan@hp.com>
2024-03-01 13:09:59 -0600

    feat: add telescope support for harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,11 +1,41 @@
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
+end
+
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = true,
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup({})
+  end,
   keys = {
     {
       "<leader>ho",
+      function()
+        toggle_telescope(require("harpoon"):list())
+      end,
+      desc = "Open harpoon window",
+    },
+    {
+      "<leader>hl",
       function()
         local harpoon = require("harpoon")
         harpoon.ui:toggle_quick_menu(harpoon:list())
