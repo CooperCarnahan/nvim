d8c4af21377d05748d3fff388ca7b6cc0cf2f639
Cooper Carnahan <cooper.carnahan@hp.com>
2025-02-27 13:50:49 -0600

    feat: migrate additional plugins to snacks.picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,9 @@
 return {
   "folke/snacks.nvim",
   keys = {
-    { "<C-p>", "<cmd>lua require('snacks').picker('files')<cr>", desc = "Find files" },
+    { "<C-p>", "<cmd>lua require('snacks').picker('smart')<cr>", desc = "Find files (smart)" },
+    { "<C-t>", "<cmd>lua require('snacks').picker('treesitter')<cr>", desc = "Find treesitter symbols" },
+    { "<leader>sz", "<cmd>lua require('snacks').picker('zoxide')<cr>", desc = "Find Zoxide" },
   },
   opts = {
     scroll = { enabled = false },
