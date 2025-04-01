fc44f9b80b3ae37ab72c0e40988b66629e4aa14b
Cooper Carnahan <cooper.carnahan@hp.com>
2025-04-01 10:56:41 -0500

    feat: lsp_definitions filters out duplicates from multiple lsp sources
    Was an issue when using clangd + ctags_lsp at the same time

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,5 @@
+local seen = {} -- used to skip duplicate entries in lsp_definitions
+
 return {
   "folke/snacks.nvim",
   keys = {
@@ -7,5 +9,33 @@ return {
   },
   opts = {
     scroll = { enabled = false },
+    picker = {
+      sources = {
+        lsp_definitions = {
+          layout = { preset = "ivy" },
+          matcher = { frecency = true, history_bonus = true },
+          unique_lines = true,
+          filter = {
+            filter = function(item, _)
+              if not item.file or not item.line then
+                return false -- Return false for invalid items
+              end
+
+              -- skip stuff we've already seen
+              local key = item.file .. ":" .. item.line
+              if not seen[key] then
+                seen[key] = true
+                return true
+              end
+              return false
+            end,
+          },
+          on_close = function()
+            -- Clear the `seen` table efficiently when the picker is closed
+            seen = {}
+          end,
+        },
+      },
+    },
   },
 }
